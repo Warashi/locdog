c9da060f4e4f5e619ed1e5f122b2778cec3db35f
Shinnosuke Sawada <6warashi9@gmail.com>
2019-05-21 01:25:54 +0900

    再実装

--- main.go
+++ main.go
@@ -12,48 +12,152 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+type Duration time.Duration
+
+func (d *Duration) UnmarshalText(text []byte) error {
+	td, err := time.ParseDuration(string(text))
+	if err != nil {
+		return err
+	}
+	*d = Duration(td)
+	return nil
+}
+
+func (d Duration) Value() time.Duration {
+	return time.Duration(d)
+}
+
 type Config struct {
-	DefaultInterval  int64    `json:"default_interval"`
-	DefaultThreshold int64    `json:"default_threshold"`
+	DefaultInterval  Duration `json:"default_interval"`
+	DefaultThreshold Duration `json:"default_threshold"`
 	DefaultAlertCmd  []string `json:"default_alert_cmd"`
+	DefaultTimeout   Duration `json:"default_timeout"`
 	Targets          []struct {
-		Interval  int64    `json:"interval"`
-		Threshold int64    `json:"threshold"`
+		Name      string   `json:"name"`
+		Interval  Duration `json:"interval"`
+		Threshold Duration `json:"threshold"`
+		Timeout   Duration `json:"timeout"`
 		AlertCmd  []string `json:"alert_cmd"`
+		NoDataCmd []string `json:"no_data_cmd"`
 		WatchCmd  []string `json:"watch_cmd"`
 	} `json:"targets"`
 }
 
-type Runner struct {
-	lastSuccess time.Time
-	interval    time.Duration
-	threshold   time.Duration
-	alertCmd    []string
-	watchCmd    []string
+func (c *Config) FillWithDefaults() {
+	for i := range c.Targets {
+		if c.Targets[i].Interval == 0 {
+			c.Targets[i].Interval = c.DefaultInterval
+		}
+		if c.Targets[i].Threshold == 0 {
+			c.Targets[i].Threshold = c.DefaultThreshold
+		}
+		if len(c.Targets[i].AlertCmd) == 0 {
+			c.Targets[i].AlertCmd = c.DefaultAlertCmd
+		}
+		if c.Targets[i].Timeout == 0 {
+			c.Targets[i].Timeout = c.DefaultTimeout
+		}
+	}
 }
 
-func (r *Runner) Run(ctx context.Context) error {
-	r.lastSuccess = time.Now()
-	for {
+type Target struct {
+	Name        string
+	LastSuccess time.Time
+	LastData    time.Time
+	Threshold   time.Duration
+	AlertCmd    []string
+	NoDataCmd   []string
+}
+
+type Result struct {
+	Name      string
+	Succeeded bool
+	Timestamp time.Time
+}
+
+type Server struct {
+	Targets map[string]*Target
+	ResCh   chan Result
+}
+
+func (s *Server) Register(t *Target) {
+	if s.Targets == nil {
+		s.Targets = make(map[string]*Target)
+	}
+	s.Targets[t.Name] = t
+}
+
+func (r *Server) Run(ctx context.Context) error {
+	for res := range r.ResCh {
+		now := time.Now()
+		tgt := r.Targets[res.Name]
+		if tgt.LastSuccess.After(res.Timestamp) {
+			tgt.LastData = res.Timestamp
+			if res.Succeeded {
+				tgt.LastSuccess = res.Timestamp
+			}
+		}
+		if tgt.NoDataCmd != nil && now.Sub(tgt.LastData) > tgt.Threshold {
+			go func(cmd []string) {
+				exec.Command(cmd[0], cmd[1:]...).Run()
+			}(tgt.NoDataCmd)
+			continue
+		}
+		if tgt.AlertCmd != nil && now.Sub(tgt.LastSuccess) > tgt.Threshold {
+			go func(cmd []string) {
+				exec.Command(cmd[0], cmd[1:]...).Run()
+			}(tgt.AlertCmd)
+		}
+	}
+	return nil
+}
+
+func (r *Server) NoDataWatch(ctx context.Context) error {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for range ticker.C {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		default:
-			now := time.Now()
-			cmd := exec.CommandContext(ctx, r.watchCmd[0], r.watchCmd[1:]...)
-			if err := cmd.Run(); err == nil {
-				r.lastSuccess = now
+			for _, t := range r.Targets {
+				r.ResCh <- Result{Name: t.Name}
 			}
-
-			if now.Sub(r.lastSuccess) > r.threshold {
-				cmd := exec.CommandContext(ctx, r.alertCmd[0], r.alertCmd[1:]...)
-				if err := cmd.Run(); err != nil {
-					return err
-				}
-			}
-			time.Sleep(r.interval)
 		}
 	}
+	return nil
+}
+
+type Watcher struct {
+	Name     string
+	ResCh    chan<- Result
+	Interval time.Duration
+	Timeout  time.Duration
+	WatchCmd []string
+}
+
+func (r *Watcher) Run(ctx context.Context) error {
+	ticker := time.NewTicker(r.Interval)
+	defer ticker.Stop()
+	for range ticker.C {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			go func() {
+				ctx, cancel := context.WithTimeout(ctx, r.Timeout)
+				defer cancel()
+				now := time.Now()
+				err := exec.CommandContext(ctx, r.WatchCmd[0], r.WatchCmd[1:]...).Run()
+				r.ResCh <- Result{
+					Name:      r.Name,
+					Succeeded: err == nil,
+					Timestamp: now,
+				}
+			}()
+		}
+	}
+	return nil
 }
 
 func main() {
@@ -72,37 +176,34 @@ func main() {
 	if err := json.NewDecoder(f).Decode(&cfg); err != nil {
 		log.Panic(err)
 	}
+	cfg.FillWithDefaults()
 
 	g, ctx := errgroup.WithContext(context.Background())
 
-	for i := range cfg.Targets {
+	s := new(Server)
+	ResCh := make(chan Result)
+	s.ResCh = ResCh
+	for _, t := range cfg.Targets {
+		s.Register(&Target{
+			Name:      t.Name,
+			Threshold: t.Threshold.Value(),
+			AlertCmd:  t.AlertCmd,
+			NoDataCmd: t.NoDataCmd,
+		})
 		g.Go(func() error {
-			var interval, threshold int64
-			var alertCmd []string
-			if cfg.Targets[i].Interval == 0 {
-				interval = cfg.DefaultInterval
-			} else {
-				interval = cfg.Targets[i].Interval
+			w := &Watcher{
+				Name:     t.Name,
+				Interval: t.Interval.Value(),
+				Timeout:  t.Timeout.Value(),
+				WatchCmd: t.WatchCmd,
+				ResCh:    ResCh,
 			}
-			if cfg.Targets[i].Threshold == 0 {
-				threshold = cfg.DefaultThreshold
-			} else {
-				threshold = cfg.Targets[i].Threshold
-			}
-			if len(cfg.Targets[i].AlertCmd) == 0 {
-				alertCmd = cfg.DefaultAlertCmd
-			} else {
-				alertCmd = cfg.Targets[i].AlertCmd
-			}
-			r := &Runner{
-				interval:  time.Duration(interval) * time.Second,
-				threshold: time.Duration(threshold) * time.Second,
-				alertCmd:  alertCmd,
-				watchCmd:  cfg.Targets[i].WatchCmd,
-			}
-			return r.Run(ctx)
+			return w.Run(ctx)
 		})
 	}
+	g.Go(func() error {
+		return s.NoDataWatch(ctx)
+	})
 
 	if err := g.Wait(); err != nil {
 		log.Panic(err)
